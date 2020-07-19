852dec09f20d4d0a46085585d014167512705081
yumi1103 <tanisi.1103@gmail.com>
2020-07-19 17:40:44 +0900

    * test_app_table.sln: * Main.cs: * Info.plist: * AppDelegate.cs: * Main.storyboard: * ViewController.cs: * Entitlements.plist: * test_app_table.csproj: * ViewController.designer.cs: * Contents.json: * Contents.json: * AppIcon-32.png: * AppIcon-16.png: * AppIcon-256.png: * AppIcon-128.png: * AppIcon-512.png: * AppIcon-32@2x.png: * AppIcon-16@2x.png: * AppIcon-512@2x.png: * AppIcon-128@2x.png: * AppIcon-256@2x.png:
    * .gitignore: feat:　create repository

---
+++ test_app_table/Main.cs
@@ -0,0 +1,13 @@
+﻿using AppKit;
+
+namespace test_app_table
+{
+    static class MainClass
+    {
+        static void Main(string[] args)
+        {
+            NSApplication.Init();
+            NSApplication.Main(args);
+        }
+    }
+}
